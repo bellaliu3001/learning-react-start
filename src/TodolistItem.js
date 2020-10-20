f2474aee257f401b6e59048a97f3221ea63ad2d2
bellaliu3001 <lhsblue@hotmail.com>
2020-10-20 22:47:41 +1100

    introduced propTypes and defaultProps

--- src/TodolistItem.js
+++ src/TodolistItem.js
@@ -1,12 +1,23 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import PropTypes from 'prop-types'
+
 
 export default class TodolistItem extends Component {
+  static propTypes = {
+    item: PropTypes.string,
+    handleItemDelete: PropTypes.func,
+    index: PropTypes.number,
+    test: PropTypes.string.isRequired,
+  }
+  static defaultProps = {
+    test: "ListItem",
+  }
   handleClick = () => {
     this.props.handleItemDelete(this.props.index);
   }
   render() {
     return (
-      <li onClick={this.handleClick}>{this.props.item}</li>
+      <li onClick={this.handleClick}>{this.props.test} - {this.props.item}</li>
     )
   }
 }
