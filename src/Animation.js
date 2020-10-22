19f7535af823766ca9aede902d1d1c9a7a303e45
bellaliu3001 <lhsblue@hotmail.com>
2020-10-22 21:59:21 +1100

    introduced csstransition

--- src/Animation.js
+++ src/Animation.js
@@ -1,4 +1,5 @@
 import React, { Component, Fragment } from 'react'
+import { CSSTransition } from 'react-transition-group';
 
 export default class Animation extends Component {
   constructor(props){
@@ -12,11 +13,27 @@ export default class Animation extends Component {
       show: !this.state.show
     }))
   }
+  handleToggle2 = () => {
+    this.setState(() => ({
+      show: !this.state.show
+    }))
+  }
   render() {
     return (
       <Fragment>
         <div className={this.state.show ? 'show' : 'hide'}>hello</div>
         <button onClick={this.handleToggle}>Toggle</button>
+
+        <CSSTransition
+          in={this.state.show}
+          timeout={1000}
+          classNames='fade'
+          onEntered={(el) => {el.style.color = 'red'}}
+          appear={true}
+        >
+          <div>with CSSTransition</div>
+        </CSSTransition>
+        <button onClick={this.handleToggle2}>Toggle</button>
       </Fragment>
     )
   }
